2888498132e5ada9ca672e9d86203619153f972e
D <924004+daw42@users.noreply.github.com>
2024-03-17 11:21:24 -0700

    Remove 2d stuff

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,5 @@ window.addEventListener("DOMContentLoaded", main);
 
 function main() : void {
     // Get the canvas element
-    const canvas : HTMLCanvasElement     = document.querySelector('#main-canvas');
-    
-    // Get the 2D drawing context from the canvas element
-    const ctx : CanvasRenderingContext2D = canvas.getContext("2d");
-
-    // Get the element containing the canvas
-    const container : HTMLElement = document.querySelector('#canvas-container');
-    
-    // Resize the canvas to fit the container
-    canvas.width = container.clientWidth;
-    canvas.height = container.clientHeight;
-
-    // Draw some 2D graphics
-    ctx.fillStyle = "rgb(255,0,0)";
-    ctx.fillRect(100, 100, 400, 175);
-
-    ctx.fillStyle = "black";
-    ctx.font = "48px serif";
-    ctx.fillText("Hello World!", 175, 200);
+    const canvas : HTMLCanvasElement = document.querySelector('#main-canvas');
 }
