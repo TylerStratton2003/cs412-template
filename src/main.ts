f89e2ab0625d990dd51dcabd3ebf43cb5c0b0f0c
D <924004+daw42@users.noreply.github.com>
2024-03-17 20:43:44 -0700

    Updated

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
+import * as THREE from 'three'
+import {GUI} from 'lil-gui';
+
 // A starter template for a Typescript browser application that renders
 // to a canvas element.
 
-// Call the main method after the DOM is loaded
 window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("resize", resize, false);
+
+let canvas : HTMLCanvasElement;
 
 function main() : void {
     // Get the canvas element
-    const canvas : HTMLCanvasElement = document.querySelector('#main-canvas');
+    canvas = document.querySelector('#main-canvas');
+
+    resize();  // Call directly once to initially size the canvas
+}
+
+function resize() {
+    // Get the dimensions of the canvas' parent
+    const canvasContainer = canvas.parentElement;
+    const w = canvasContainer.clientWidth;
+    const h = canvasContainer.clientHeight;
+
+    // Update camera's aspect ratio and projection matrix, then set the renderer's 
+    // viewport matrix with renderer.setSize() (this will also resize the canvas)
 }
