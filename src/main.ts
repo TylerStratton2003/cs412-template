5b6a573d4c4def7cc61b946adc13cecb4e97b671
TylerStratton2003 <tyler.stratton@plu.edu>
2024-05-01 13:38:17 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,47 @@ window.addEventListener("DOMContentLoaded", main);
 window.addEventListener("resize", resize);
 
 let canvas : HTMLCanvasElement;
+var scene: THREE.Scene;
+var renderer: THREE.Renderer;
+var camera: THREE.Camera;
+var box : THREE.Mesh;
+
+const boxPosition = {
+    positionX: 0
+};
 
 function main() : void {
     // Get the canvas element
     canvas = document.querySelector('#main-canvas');
 
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.5, 2000);
+    renderer = new THREE.WebGLRenderer({canvas});
+    camera.position.z = 5;
+
+    const boxGeom = new THREE.BoxGeometry(1, 1, 1);
+    const boxMaterial = new THREE.MeshBasicMaterial({color: 0xffff00});
+    box = new THREE.Mesh(boxGeom, boxMaterial);
+
+    const gui = new GUI();
+    gui.add(boxPosition, "positionX", -3.0, 3.0, 0.1);
+
+    scene.add(box);
+
+
     resize();  // Call directly once to initially size the canvas
+    requestAnimationFrame(draw);
+}
+
+function draw(time: number) {
+    const speed = 0.1;
+    const angle = (time * 0.001) * speed;
+    box.position.x = boxPosition.positionX;
+    box.rotation.x = angle;
+    box.rotation.y = angle;
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(draw);
 }
 
 function resize() {
